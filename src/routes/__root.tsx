d616caec34c479a89940a5697e472ad49a3e7a80
Jon Monarrez <135302142+j0n-m@users.noreply.github.com>
2024-11-08 15:16:53 -0600

    Readying for deployment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,29 @@
 import { createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import MissingPage from "../pages/404/MissingPage";
 import App from "../App";
 import ErrorPage from "../pages/Error/ErrorPage";
+import React, { Suspense } from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <App />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
   notFoundComponent: () => <MissingPage />,
