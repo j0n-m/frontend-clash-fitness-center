1f6a89447ac126324ec30f0c0cdd340dc3cdbbb9
Jon Monarrez <135302142+j0n-m@users.noreply.github.com>
2024-11-08 15:06:33 -0600

    change styling preference

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -5,7 +5,9 @@ type LandingHeaderProps = {
 function LandingHeader({ text }: LandingHeaderProps) {
   return (
     <div className="landing-header about-us-bg h-[35svh] flex justify-center items-end">
-      <h1 className="text-white-primary font-bold text-2xl p-4">{text}</h1>
+      <h1 className="text-white-primary font-bold text-2xl md:text-3xl p-4">
+        {text}
+      </h1>
     </div>
   );
 }
